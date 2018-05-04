15833bb8a2c8e02594495b5f72707f2c9b658390
Steven Chen <ysteven.chen2@gmail.com>
2018-05-04 23:01:54 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,4 +1,15 @@
+
 def my_select(collection)
  # your code here!
-collection.select { |x|x.even?}
+i=0
+even=[]
+while i <collection.length
+  if yield(collection[i])
+    even << collection[i]
+  end
+  i+=1
 end
+even
+end
+
+#block my_select(collection) { |x| x.even?}
